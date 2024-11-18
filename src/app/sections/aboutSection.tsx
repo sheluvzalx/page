c2173e977980cf25a38de37b816842d4839c01ca
spectre <sheluvzalx@users.noreply.github.com>
2024-11-17 12:29:31 -1200

    some fix

--- src/app/sections/aboutSection.tsx
+++ src/app/sections/aboutSection.tsx
@@ -2,15 +2,15 @@ import styles from '@/app/page.module.css'
 export default function aboutSection() {
     return (
         <div id='about' className={styles.card}>
+              <div>👾 | Discord Bots Coder</div>
+            <div>20 <span>y/o</span></div> <div>💲   | Flexible Prices</div> <div>Frendly</div> <div>Responsible Dude</div>
             
-            <div>20 <span>y/o</span></div> <div>Frendly</div> <div>Responsible Dude</div>
-            <div>Im <span> From</span> Dominican republic</div>
-            <div>Trust <span>issues</span>(if u dont pay first u dont get my services)</div>
-            <div>👾 | Discord Bots Coder</div>
-            <div>🚀 | Payed Helper </div>
-            <div>🔧 | Final Project's Coder</div>
-            <div>😄 | Front-end web dev</div>
-            <div>💲   | Flexible Prices</div>
+            <div>🚀 | Payed Helper </div><div>Im <span> From</span> Dominican republic</div><div>😄 | Front-end web dev</div>
+            <div>🔧 | Final Project's Coder</div><div>Trust <span>issues</span>(if u dont pay first u dont get my services)</div>
+           
+           
+            
+           
             
            </div>
     )
